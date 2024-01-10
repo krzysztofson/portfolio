e5f7d655d0cfafdfc8c3726284d6ebe46fb6d252
Krzysztof Dąbrowski <47756218+krzysztofson@users.noreply.github.com>
2024-01-10 11:07:58 +0100

    New files

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./dist/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'main': ['Poppins', 'sans-serif'],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
